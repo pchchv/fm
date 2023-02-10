6ba17bf367349ee16313f24d52703ce81a207e1e
pchchv <ipchchv@gmail.com>
2023-02-10 12:42:26 +0300

    Implement a readPairs and replaceTilde funcs

--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module github.com/pchchv/fm
 go 1.20
 
 require (
+	github.com/gdamore/tcell/v2 v2.5.4
 	github.com/pchchv/golog v1.0.1
 	golang.org/x/sys v0.5.0
+)
+
+require (
+	github.com/djherbis/times v1.5.0
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell v1.4.0
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.5.0 // indirect
 )
--- helpers.go
+++ helpers.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"strings"
 	"unicode"
 )
@@ -22,4 +25,78 @@ func splitWord(s string) (word, rest string) {
 	word = s[0:ind]
 	rest = strings.TrimLeftFunc(s[ind:], unicode.IsSpace)
 	return
+}
+
+// ReadPairs reads pairs of lines, separated by spaces, on each line.
+// Single or double quotes can be used to escape spaces.
+// Hash characters can be used to add a comment before the end of a line.
+// Leading and trailing spaces are truncated. Blank lines are skipped.
+func readPairs(r io.Reader) ([][]string, error) {
+	var pairs [][]string
+
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		line := s.Text()
+
+		squote, dquote := false, false
+		for i := 0; i < len(line); i++ {
+			if line[i] == '\'' && !dquote {
+				squote = !squote
+			} else if line[i] == '"' && !squote {
+				dquote = !dquote
+			}
+			if !squote && !dquote && line[i] == '#' {
+				line = line[:i]
+				break
+			}
+		}
+
+		line = strings.TrimSpace(line)
+
+		if line == "" {
+			continue
+		}
+
+		squote, dquote = false, false
+		pair := strings.FieldsFunc(line, func(r rune) bool {
+			if r == '\'' && !dquote {
+				squote = !squote
+			} else if r == '"' && !squote {
+				dquote = !dquote
+			}
+			return !squote && !dquote && unicode.IsSpace(r)
+		})
+
+		if len(pair) != 2 {
+			return nil, fmt.Errorf("expected pair but found: %s", s.Text())
+		}
+
+		for i := 0; i < len(pair); i++ {
+			squote, dquote = false, false
+			buf := make([]rune, 0, len(pair[i]))
+			for _, r := range pair[i] {
+				if r == '\'' && !dquote {
+					squote = !squote
+					continue
+				}
+				if r == '"' && !squote {
+					dquote = !dquote
+					continue
+				}
+				buf = append(buf, r)
+			}
+			pair[i] = string(buf)
+		}
+
+		pairs = append(pairs, pair)
+	}
+
+	return pairs, nil
+}
+
+func replaceTilde(s string) string {
+	if strings.HasPrefix(s, "~") {
+		s = strings.Replace(s, "~", genUser.HomeDir, 1)
+	}
+	return s
 }
