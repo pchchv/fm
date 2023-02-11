d8700ca09d9f1e3e94a8648a65b960b92b27e746
pchchv <ipchchv@gmail.com>
2023-02-11 08:49:00 +0300

    Implement a mapExpr type

--- parser.go
+++ parser.go
@@ -18,10 +18,19 @@ type setExpr struct {
 	val string
 }
 
+type mapExpr struct {
+	keys string
+	expr expr
+}
+
 func (e *callExpr) String() string {
 	return fmt.Sprintf("%s -- %s", e.name, e.args)
 }
 
 func (e *setExpr) String() string {
 	return fmt.Sprintf("set %s %s", e.opt, e.val)
+}
+
+func (e *mapExpr) String() string {
+	return fmt.Sprintf("map %s %s", e.keys, e.expr)
 }
