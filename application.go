9862cc8e6250ab44f15f9e90010ac914a3e3e630
pchchv <ipchchv@gmail.com>
2023-02-12 00:38:54 +0300

    Implement a saveFiles func

--- application.go
+++ application.go
@@ -79,46 +79,6 @@ func (app *app) quit() {
 	}
 }
 
-func loadFiles() (list []string, cp bool, err error) {
-	files, err := os.Open(genFilesPath)
-	if os.IsNotExist(err) {
-		err = nil
-		return
-	}
-	if err != nil {
-		err = fmt.Errorf("opening file selections file: %s", err)
-		return
-	}
-	defer files.Close()
-
-	s := bufio.NewScanner(files)
-
-	s.Scan()
-
-	switch s.Text() {
-	case "copy":
-		cp = true
-	case "move":
-		cp = false
-	default:
-		err = fmt.Errorf("unexpected option to copy file(s): %s", s.Text())
-		return
-	}
-
-	for s.Scan() && s.Text() != "" {
-		list = append(list, s.Text())
-	}
-
-	if s.Err() != nil {
-		err = fmt.Errorf("scanning file list: %s", s.Err())
-		return
-	}
-
-	log.Printf("loading files: %v", list)
-
-	return
-}
-
 func (app *app) readHistory() error {
 	f, err := os.Open(genHistoryPath)
 	if os.IsNotExist(err) {
@@ -184,6 +144,72 @@ func (app *app) writeHistory() error {
 		if err != nil {
 			return fmt.Errorf("writing history file: %s", err)
 		}
+	}
+
+	return nil
+}
+
+func loadFiles() (list []string, cp bool, err error) {
+	files, err := os.Open(genFilesPath)
+	if os.IsNotExist(err) {
+		err = nil
+		return
+	}
+	if err != nil {
+		err = fmt.Errorf("opening file selections file: %s", err)
+		return
+	}
+	defer files.Close()
+
+	s := bufio.NewScanner(files)
+
+	s.Scan()
+
+	switch s.Text() {
+	case "copy":
+		cp = true
+	case "move":
+		cp = false
+	default:
+		err = fmt.Errorf("unexpected option to copy file(s): %s", s.Text())
+		return
+	}
+
+	for s.Scan() && s.Text() != "" {
+		list = append(list, s.Text())
+	}
+
+	if s.Err() != nil {
+		err = fmt.Errorf("scanning file list: %s", s.Err())
+		return
+	}
+
+	log.Printf("loading files: %v", list)
+
+	return
+}
+
+func saveFiles(list []string, cp bool) error {
+	if err := os.MkdirAll(filepath.Dir(genFilesPath), os.ModePerm); err != nil {
+		return fmt.Errorf("creating data directory: %s", err)
+	}
+
+	files, err := os.Create(genFilesPath)
+	if err != nil {
+		return fmt.Errorf("opening file selections file: %s", err)
+	}
+	defer files.Close()
+
+	log.Printf("saving files: %v", list)
+
+	if cp {
+		fmt.Fprintln(files, "copy")
+	} else {
+		fmt.Fprintln(files, "move")
+	}
+
+	for _, f := range list {
+		fmt.Fprintln(files, f)
 	}
 
 	return nil
