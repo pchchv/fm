c2e225d0ee7b2833abe852947532e1210f49cafb
pchchv <ipchchv@gmail.com>
2023-02-11 09:29:36 +0300

    Implement an os funcs

--- os.go
+++ os.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"os/user"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"syscall"
 
 	"golang.org/x/sys/unix"
 )
 
 var (
+	envOpener = os.Getenv("OPENER")
+	envEditor = os.Getenv("EDITOR")
+	envPager  = os.Getenv("PAGER")
+	envShell  = os.Getenv("SHELL")
+
 	genDefaultShell      = "sh"
 	genDefaultShellFlag  = "-c"
 	genDefaultSocketProt = "unix"
@@ -23,12 +35,189 @@ var (
 	genHistoryPath string
 )
 
+func init() {
+	if envOpener == "" {
+		if runtime.GOOS == "darwin" {
+			envOpener = "open"
+		} else {
+			envOpener = "xdg-open"
+		}
+	}
+
+	if envEditor == "" {
+		envEditor = "vi"
+	}
+
+	if envPager == "" {
+		envPager = "less"
+	}
+
+	if envShell == "" {
+		envShell = "sh"
+	}
+
+	u, err := user.Current()
+	if err != nil {
+		// When the user is not in /etc/passwd (for e.g. LDAP) and CGO_ENABLED=1 in go env,
+		// the cgo implementation of user.Current() fails even when HOME and USER are set.
+
+		log.Printf("user: %s", err)
+		if os.Getenv("HOME") == "" {
+			panic("$HOME variable is empty or not set")
+		}
+		if os.Getenv("USER") == "" {
+			panic("$USER variable is empty or not set")
+		}
+		u = &user.User{
+			Username: os.Getenv("USER"),
+			HomeDir:  os.Getenv("HOME"),
+		}
+	}
+	genUser = u
+
+	config := os.Getenv("XDG_CONFIG_HOME")
+	if config == "" {
+		config = filepath.Join(genUser.HomeDir, ".config")
+	}
+
+	genConfigPaths = []string{
+		filepath.Join("/etc", "lf", "lfrc"),
+		filepath.Join(config, "lf", "lfrc"),
+	}
+
+	genColorsPaths = []string{
+		filepath.Join("/etc", "lf", "colors"),
+		filepath.Join(config, "lf", "colors"),
+	}
+
+	genIconsPaths = []string{
+		filepath.Join("/etc", "lf", "icons"),
+		filepath.Join(config, "lf", "icons"),
+	}
+
+	data := os.Getenv("XDG_DATA_HOME")
+	if data == "" {
+		data = filepath.Join(genUser.HomeDir, ".local", "share")
+	}
+
+	genFilesPath = filepath.Join(data, "lf", "files")
+	genMarksPath = filepath.Join(data, "lf", "marks")
+	genTagsPath = filepath.Join(data, "lf", "tags")
+	genHistoryPath = filepath.Join(data, "lf", "history")
+
+	runtime := os.Getenv("XDG_RUNTIME_DIR")
+	if runtime == "" {
+		runtime = os.TempDir()
+	}
+
+	genDefaultSocketPath = filepath.Join(runtime, fmt.Sprintf("lf.%s.sock", genUser.Username))
+}
+
 func detachedCommand(name string, arg ...string) *exec.Cmd {
 	cmd := exec.Command(name, arg...)
 	cmd.SysProcAttr = &unix.SysProcAttr{Setsid: true}
 	return cmd
 }
 
+func shellCommand(s string, args []string) *exec.Cmd {
+	if len(genOpts.ifs) != 0 {
+		s = fmt.Sprintf("IFS='%s'; %s", genOpts.ifs, s)
+	}
+
+	args = append([]string{genOpts.shellflag, s, "--"}, args...)
+
+	args = append(genOpts.shellopts, args...)
+
+	return exec.Command(genOpts.shell, args...)
+}
+
+func shellSetPG(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &unix.SysProcAttr{Setpgid: true}
+}
+
+func shellKill(cmd *exec.Cmd) error {
+	pgid, err := unix.Getpgid(cmd.Process.Pid)
+	if err == nil && cmd.Process.Pid == pgid {
+		// kill the process group
+		err = unix.Kill(-pgid, 15)
+		if err == nil {
+			return nil
+		}
+	}
+	return cmd.Process.Kill()
+}
+
+func setDefaults() {
+	genOpts.cmds["open"] = &execExpr{"&", `$OPENER "$f"`}
+	genOpts.keys["e"] = &execExpr{"$", `$EDITOR "$f"`}
+	genOpts.keys["i"] = &execExpr{"$", `$PAGER "$f"`}
+	genOpts.keys["w"] = &execExpr{"$", "$SHELL"}
+
+	genOpts.cmds["doc"] = &execExpr{"$", "lf -doc | $PAGER"}
+	genOpts.keys["<f-1>"] = &callExpr{"doc", nil, 1}
+}
+
 func setUserUmask() {
 	unix.Umask(0o077)
+}
+
+func isExecutable(f os.FileInfo) bool {
+	return f.Mode()&0o111 != 0
+}
+
+func isHidden(f os.FileInfo, path string, hiddenfiles []string) bool {
+	hidden := false
+	for _, pattern := range hiddenfiles {
+		matched := matchPattern(strings.TrimPrefix(pattern, "!"), f.Name(), path)
+		if strings.HasPrefix(pattern, "!") && matched {
+			hidden = false
+		} else if matched {
+			hidden = true
+		}
+	}
+	return hidden
+}
+
+func userName(f os.FileInfo) string {
+	if stat, ok := f.Sys().(*syscall.Stat_t); ok {
+		if u, err := user.LookupId(fmt.Sprint(stat.Uid)); err == nil {
+			return fmt.Sprintf("%v ", u.Username)
+		}
+	}
+	return ""
+}
+
+func groupName(f os.FileInfo) string {
+	if stat, ok := f.Sys().(*syscall.Stat_t); ok {
+		if g, err := user.LookupGroupId(fmt.Sprint(stat.Gid)); err == nil {
+			return fmt.Sprintf("%v ", g.Name)
+		}
+	}
+	return ""
+}
+
+func linkCount(f os.FileInfo) string {
+	if stat, ok := f.Sys().(*syscall.Stat_t); ok {
+		return fmt.Sprintf("%v ", stat.Nlink)
+	}
+	return ""
+}
+
+func errCrossDevice(err error) bool {
+	return err.(*os.LinkError).Err.(unix.Errno) == unix.EXDEV
+}
+
+func exportFiles(f string, fs []string, pwd string) {
+	envFile := f
+	envFiles := strings.Join(fs, genOpts.filesep)
+
+	os.Setenv("f", envFile)
+	os.Setenv("fs", envFiles)
+	os.Setenv("PWD", pwd)
+
+	if len(fs) == 0 {
+		os.Setenv("fx", envFile)
+	} else {
+		os.Setenv("fx", envFiles)
+	}
 }
--- os_windows.go
+++ os_windows.go
@@ -3,6 +3,13 @@ package main
 import "golang.org/x/sys/windows"
 
 var (
+	envOpener = os.Getenv("OPENER")
+	envEditor = os.Getenv("EDITOR")
+	envPager  = os.Getenv("PAGER")
+	envShell  = os.Getenv("SHELL")
+
+	envPathExt = os.Getenv("PATHEXT")
+
 	genDefaultShell      = "cmd"
 	genDefaultShellFlag  = "/c"
 	genDefaultSocketProt = "tcp"
@@ -18,10 +25,145 @@ var (
 	genHistoryPath string
 )
 
+func init() {
+	if envOpener == "" {
+		envOpener = `start ""`
+	}
+
+	if envEditor == "" {
+		envEditor = "notepad"
+	}
+
+	if envPager == "" {
+		envPager = "more"
+	}
+
+	if envShell == "" {
+		envShell = "cmd"
+	}
+
+	u, err := user.Current()
+	if err != nil {
+		log.Printf("user: %s", err)
+	}
+	genUser = u
+
+	// remove domain prefix
+	genUser.Username = strings.Split(genUser.Username, `\`)[1]
+
+	data := os.Getenv("LOCALAPPDATA")
+
+	genConfigPaths = []string{
+		filepath.Join(os.Getenv("ProgramData"), "lf", "lfrc"),
+		filepath.Join(data, "lf", "lfrc"),
+	}
+
+	genColorsPaths = []string{
+		filepath.Join(os.Getenv("ProgramData"), "lf", "colors"),
+		filepath.Join(data, "lf", "colors"),
+	}
+
+	genIconsPaths = []string{
+		filepath.Join(os.Getenv("ProgramData"), "lf", "icons"),
+		filepath.Join(data, "lf", "icons"),
+	}
+
+	genFilesPath = filepath.Join(data, "lf", "files")
+	genMarksPath = filepath.Join(data, "lf", "marks")
+	genTagsPath = filepath.Join(data, "lf", "tags")
+	genHistoryPath = filepath.Join(data, "lf", "history")
+}
+
 func detachedCommand(name string, arg ...string) *exec.Cmd {
 	cmd := exec.Command(name, arg...)
 	cmd.SysProcAttr = &windows.SysProcAttr{CreationFlags: 8}
 	return cmd
 }
 
+func shellCommand(s string, args []string) *exec.Cmd {
+	args = append([]string{gOpts.shellflag, s}, args...)
+
+	args = append(gOpts.shellopts, args...)
+
+	return exec.Command(gOpts.shell, args...)
+}
+
+func shellSetPG(cmd *exec.Cmd) {
+}
+
+func shellKill(cmd *exec.Cmd) error {
+	return cmd.Process.Kill()
+}
+
+func setDefaults() {
+	gOpts.cmds["open"] = &execExpr{"&", "%OPENER% %f%"}
+	gOpts.keys["e"] = &execExpr{"$", "%EDITOR% %f%"}
+	gOpts.keys["i"] = &execExpr{"!", "%PAGER% %f%"}
+	gOpts.keys["w"] = &execExpr{"$", "%SHELL%"}
+
+	gOpts.cmds["doc"] = &execExpr{"!", "lf -doc | %PAGER%"}
+	gOpts.keys["<f-1>"] = &callExpr{"doc", nil, 1}
+}
+
 func setUserUmask() {}
+
+func isExecutable(f os.FileInfo) bool {
+	exts := strings.Split(envPathExt, string(filepath.ListSeparator))
+	for _, e := range exts {
+		if strings.HasSuffix(strings.ToLower(f.Name()), strings.ToLower(e)) {
+			log.Print(f.Name(), e)
+			return true
+		}
+	}
+	return false
+}
+
+func isHidden(f os.FileInfo, path string, hiddenfiles []string) bool {
+	ptr, err := windows.UTF16PtrFromString(filepath.Join(path, f.Name()))
+	if err != nil {
+		return false
+	}
+	attrs, err := windows.GetFileAttributes(ptr)
+	if err != nil {
+		return false
+	}
+	return attrs&windows.FILE_ATTRIBUTE_HIDDEN != 0
+}
+
+func userName(f os.FileInfo) string {
+	return ""
+}
+
+func groupName(f os.FileInfo) string {
+	return ""
+}
+
+func linkCount(f os.FileInfo) string {
+	return ""
+}
+
+func errCrossDevice(err error) bool {
+	return err.(*os.LinkError).Err.(windows.Errno) == 17
+}
+
+func exportFiles(f string, fs []string, pwd string) {
+	envFile := fmt.Sprintf(`"%s"`, f)
+
+	var quotedFiles []string
+	for _, f := range fs {
+		quotedFiles = append(quotedFiles, fmt.Sprintf(`"%s"`, f))
+	}
+	envFiles := strings.Join(quotedFiles, gOpts.filesep)
+
+	envPWD := fmt.Sprintf(`"%s"`, pwd)
+
+	os.Setenv("f", envFile)
+	os.Setenv("fs", envFiles)
+	os.Setenv("PWD", envPWD)
+
+	if len(fs) == 0 {
+		os.Setenv("fx", envFile)
+	} else {
+		os.Setenv("fx", envFiles)
+	}
+}
