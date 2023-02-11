1c66d485ab8a29ccd38419596e2a67c0c5fa0082
pchchv <ipchchv@gmail.com>
2023-02-11 08:46:28 +0300

    Implement a callExpr type

--- parser.go
+++ parser.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 type expr interface {
 	String() string
 	eval(app *app, args []string)
+}
+
+type callExpr struct {
+	name  string
+	args  []string
+	count int
+}
+
+func (e *callExpr) String() string {
+	return fmt.Sprintf("%s -- %s", e.name, e.args)
 }
