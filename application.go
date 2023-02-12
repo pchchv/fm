d7b96e65332efdfe44ea8fc8a522bdc907c8078b
pchchv <ipchchv@gmail.com>
2023-02-12 09:11:02 +0300

    Implement a readFile, loop and runShell methods

--- application.go
+++ application.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -12,6 +11,8 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/pchchv/golog"
 )
 
 type cmdItem struct {
@@ -64,16 +65,16 @@ func newApp(ui *ui, nav *nav) *app {
 func (app *app) quit() {
 	if genOpts.history {
 		if err := app.writeHistory(); err != nil {
-			log.Printf("writing history file: %s", err)
+			golog.Info("writing history file: %s", err)
 		}
 	}
 	if !genSingleMode {
 		if err := remote(fmt.Sprintf("drop %d", genClientID)); err != nil {
-			log.Printf("dropping connection: %s", err)
+			golog.Info("dropping connection: %s", err)
 		}
 		if genOpts.autoquit {
 			if err := remote("quit"); err != nil {
-				log.Printf("auto quitting server: %s", err)
+				golog.Info("auto quitting server: %s", err)
 			}
 		}
 	}
@@ -149,6 +150,381 @@ func (app *app) writeHistory() error {
 	return nil
 }
 
+func (app *app) readFile(path string) {
+	golog.Info("reading file: %s", path)
+
+	f, err := os.Open(path)
+	if err != nil {
+		app.ui.echoerrf("opening file: %s", err)
+		return
+	}
+	defer f.Close()
+
+	p := newParser(f)
+
+	for p.parse() {
+		p.expr.eval(app, nil)
+	}
+
+	if p.err != nil {
+		app.ui.echoerrf("%s", p.err)
+	}
+}
+
+// loop is the main event loop of the application.
+// Expressions are read from the client and server in
+// separate goroutines and sent here over channels for evaluation.
+// Similarly, directories and regular files are also read in
+// separate goroutines and sent here to be updated.
+func (app *app) loop() {
+	go app.nav.previewLoop(app.ui)
+	go app.nav.dirPreviewLoop(app.ui)
+
+	var serverChan <-chan expr
+	if !genSingleMode {
+		serverChan = readExpr()
+	}
+
+	app.ui.readExpr()
+
+	if genConfigPath != "" {
+		if _, err := os.Stat(genConfigPath); !os.IsNotExist(err) {
+			app.readFile(genConfigPath)
+		} else {
+			golog.Info("config file does not exist: %s", err)
+		}
+	} else {
+		for _, path := range genConfigPaths {
+			if _, err := os.Stat(path); !os.IsNotExist(err) {
+				app.readFile(path)
+			}
+		}
+	}
+
+	for _, cmd := range genCommands {
+		p := newParser(strings.NewReader(cmd))
+
+		for p.parse() {
+			p.expr.eval(app, nil)
+		}
+
+		if p.err != nil {
+			app.ui.echoerrf("%s", p.err)
+		}
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		golog.Info("getting current directory: %s", err)
+	}
+
+	app.nav.getDirs(wd)
+	app.nav.addJumpList()
+	app.nav.init = true
+
+	if genSelect != "" {
+		go func() {
+			lstat, err := os.Lstat(genSelect)
+			if err != nil {
+				app.ui.exprChan <- &callExpr{"echoerr", []string{err.Error()}, 1}
+			} else if lstat.IsDir() {
+				app.ui.exprChan <- &callExpr{"cd", []string{genSelect}, 1}
+			} else {
+				app.ui.exprChan <- &callExpr{"select", []string{genSelect}, 1}
+			}
+		}()
+	}
+
+	for {
+		select {
+		case <-app.quitChan:
+			if app.nav.copyTotal > 0 {
+				app.ui.echoerr("quit: copy operation in progress")
+				continue
+			}
+
+			if app.nav.moveTotal > 0 {
+				app.ui.echoerr("quit: move operation in progress")
+				continue
+			}
+
+			if app.nav.deleteTotal > 0 {
+				app.ui.echoerr("quit: delete operation in progress")
+				continue
+			}
+
+			if cmd, ok := genOpts.cmds["on-quit"]; ok {
+				cmd.eval(app, nil)
+			}
+
+			app.quit()
+
+			app.nav.previewChan <- ""
+			app.nav.dirPreviewChan <- nil
+
+			golog.Info("bye!")
+
+			if genLastDirPath != "" {
+				f, err := os.Create(genLastDirPath)
+				if err != nil {
+					golog.Info("opening last dir file: %s", err)
+				}
+				defer f.Close()
+
+				_, err = f.WriteString(app.nav.currDir().path)
+				if err != nil {
+					golog.Info("writing last dir file: %s", err)
+				}
+			}
+
+			return
+		case n := <-app.nav.copyBytesChan:
+			app.nav.copyBytes += n
+			// n is usually 4096B so update roughly per 4096B x 1024 = 4MB copied
+			if app.nav.copyUpdate++; app.nav.copyUpdate >= 1024 {
+				app.nav.copyUpdate = 0
+				app.ui.draw(app.nav)
+			}
+		case n := <-app.nav.copyTotalChan:
+			app.nav.copyTotal += n
+			if n < 0 {
+				app.nav.copyBytes += n
+			}
+			if app.nav.copyTotal == 0 {
+				app.nav.copyUpdate = 0
+			}
+			app.ui.draw(app.nav)
+		case n := <-app.nav.moveCountChan:
+			app.nav.moveCount += n
+			if app.nav.moveUpdate++; app.nav.moveUpdate >= 1000 {
+				app.nav.moveUpdate = 0
+				app.ui.draw(app.nav)
+			}
+		case n := <-app.nav.moveTotalChan:
+			app.nav.moveTotal += n
+			if n < 0 {
+				app.nav.moveCount += n
+			}
+			if app.nav.moveTotal == 0 {
+				app.nav.moveUpdate = 0
+			}
+			app.ui.draw(app.nav)
+		case n := <-app.nav.deleteCountChan:
+			app.nav.deleteCount += n
+			if app.nav.deleteUpdate++; app.nav.deleteUpdate >= 1000 {
+				app.nav.deleteUpdate = 0
+				app.ui.draw(app.nav)
+			}
+		case n := <-app.nav.deleteTotalChan:
+			app.nav.deleteTotal += n
+			if n < 0 {
+				app.nav.deleteCount += n
+			}
+			if app.nav.deleteTotal == 0 {
+				app.nav.deleteUpdate = 0
+			}
+			app.ui.draw(app.nav)
+		case d := <-app.nav.dirChan:
+
+			app.nav.checkDir(d)
+
+			if genOpts.dircache {
+				prev, ok := app.nav.dirCache[d.path]
+				if ok {
+					d.ind = prev.ind
+					d.sel(prev.name(), app.nav.height)
+				}
+
+				app.nav.dirCache[d.path] = d
+			}
+
+			for i := range app.nav.dirs {
+				if app.nav.dirs[i].path == d.path {
+					app.nav.dirs[i] = d
+				}
+			}
+
+			app.nav.position()
+
+			curr, err := app.nav.currFile()
+			if err == nil {
+				if d.path == app.nav.currDir().path {
+					app.ui.loadFile(app, true)
+					if app.ui.msg == "" {
+						app.ui.loadFileInfo(app.nav)
+					}
+				}
+				if d.path == curr.path {
+					app.ui.dirPrev = d
+				}
+			}
+
+			app.ui.draw(app.nav)
+		case r := <-app.nav.regChan:
+			app.nav.checkReg(r)
+
+			app.nav.regCache[r.path] = r
+
+			curr, err := app.nav.currFile()
+			if err == nil {
+				if r.path == curr.path {
+					app.ui.regPrev = r
+				}
+			}
+
+			app.ui.draw(app.nav)
+		case ev := <-app.ui.evChan:
+			e := app.ui.readEvent(ev, app.nav)
+			if e == nil {
+				continue
+			}
+			e.eval(app, nil)
+		loop:
+			for {
+				select {
+				case ev := <-app.ui.evChan:
+					e = app.ui.readEvent(ev, app.nav)
+					if e == nil {
+						continue
+					}
+					e.eval(app, nil)
+				default:
+					break loop
+				}
+			}
+			app.ui.draw(app.nav)
+		case e := <-app.ui.exprChan:
+			e.eval(app, nil)
+			app.ui.draw(app.nav)
+		case e := <-serverChan:
+			e.eval(app, nil)
+			app.ui.draw(app.nav)
+		case <-app.ticker.C:
+			app.nav.renew()
+			app.ui.loadFile(app, false)
+			app.ui.draw(app.nav)
+		}
+	}
+}
+
+// runShell is used to run a shell command.
+// Modes are as follows:
+//
+//	Prefix  Wait  Async  Stdin  Stdout  Stderr  UI action
+//	$       No    No     Yes    Yes     Yes     Pause and then resume
+//	%       No    No     Yes    Yes     Yes     Statline for input/output
+//	!       Yes   No     Yes    Yes     Yes     Pause and then resume
+//	&       No    Yes    No     No      No      Do nothing
+func (app *app) runShell(s string, args []string, prefix string) {
+	app.nav.exportFiles()
+	app.ui.exportSizes()
+	exportOpts()
+
+	cmd := shellCommand(s, args)
+
+	var out io.Reader
+	var err error
+	switch prefix {
+	case "$", "!":
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		app.nav.previewChan <- ""
+		app.nav.dirPreviewChan <- nil
+
+		if err := app.ui.suspend(); err != nil {
+			golog.Info("suspend: %s", err)
+		}
+		defer func() {
+			if err := app.ui.resume(); err != nil {
+				app.quit()
+				os.Exit(3)
+				return
+			}
+		}()
+		defer app.nav.renew()
+
+		err = cmd.Run()
+	case "%":
+		shellSetPG(cmd)
+		if app.ui.cmdPrefix == ">" {
+			return
+		}
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			golog.Info("writing stdin: %s", err)
+		}
+		app.cmdIn = stdin
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			golog.Info("reading stdout: %s", err)
+		}
+		out = stdout
+		cmd.Stderr = cmd.Stdout
+		fallthrough
+	case "&":
+		shellSetPG(cmd)
+		err = cmd.Start()
+	}
+
+	if err != nil {
+		app.ui.echoerrf("running shell: %s", err)
+	}
+
+	switch prefix {
+	case "!":
+		anyKey()
+	}
+
+	app.ui.loadFile(app, true)
+
+	switch prefix {
+	case "%":
+		normal(app)
+		app.cmd = cmd
+		app.cmdOutBuf = nil
+		app.ui.msg = ""
+		app.ui.cmdPrefix = ">"
+
+		go func() {
+			eol := false
+			reader := bufio.NewReader(out)
+			for {
+				b, err := reader.ReadByte()
+				if err == io.EOF {
+					break
+				}
+				if eol {
+					eol = false
+					app.cmdOutBuf = nil
+				}
+				app.cmdOutBuf = append(app.cmdOutBuf, b)
+				if b == '\n' || b == '\r' {
+					eol = true
+				}
+				if reader.Buffered() > 0 {
+					continue
+				}
+				app.ui.exprChan <- &callExpr{"echo", []string{string(app.cmdOutBuf)}, 1}
+			}
+
+			if err := cmd.Wait(); err != nil {
+				golog.Info("running shell: %s", err)
+			}
+			app.cmd = nil
+			app.ui.cmdPrefix = ""
+			app.ui.exprChan <- &callExpr{"load", nil, 1}
+		}()
+	case "&":
+		go func() {
+			if err := cmd.Wait(); err != nil {
+				golog.Info("running shell: %s", err)
+			}
+		}()
+	}
+}
+
 func loadFiles() (list []string, cp bool, err error) {
 	files, err := os.Open(genFilesPath)
 	if os.IsNotExist(err) {
@@ -184,7 +560,7 @@ func loadFiles() (list []string, cp bool, err error) {
 		return
 	}
 
-	log.Printf("loading files: %v", list)
+	golog.Info("loading files: %v", list)
 
 	return
 }
@@ -200,7 +576,7 @@ func saveFiles(list []string, cp bool) error {
 	}
 	defer files.Close()
 
-	log.Printf("saving files: %v", list)
+	golog.Info("saving files: %v", list)
 
 	if cp {
 		fmt.Fprintln(files, "copy")
